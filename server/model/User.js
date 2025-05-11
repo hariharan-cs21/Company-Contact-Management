d51f1aecba043d6f268e0aed6add42b06146f6a4
Hari <hariharant.cs21@bitsathy.ac.in>
2025-05-11 11:14:01 +0530

    Schema Error cleared

--- server/model/User.js
+++ server/model/User.js
@@ -5,16 +5,27 @@ const userSchema = new Schema({
 	fullName: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
 	password: { type: String },
-	userType: { type: String, enum: ["student", "recruiter", "placement_coordinator"], required: true },
+	userType: {
+		type: String,
+		enum: ["student", "recruiter", "placement_coordinator"],
+		required: true,
+	},
 	passwordSet: { type: Boolean, default: false },
 
-	// Placement-related required fields
-	enrollmentNumber: { type: String, required: true, unique: true },
-	branch: { type: String, required: true },
-	passingYear: { type: Number, required: true },
+	enrollmentNumber: {
+		type: String,
+		required: function () { return this.userType === "student"; }
+	},
+	branch: {
+		type: String,
+		required: function () { return this.userType === "student"; }
+	},
+	passingYear: {
+		type: Number,
+		required: function () { return this.userType === "student"; }
+	},
 	resumeLink: { type: String },
 	appliedJobs: { type: [Schema.Types.ObjectId], ref: "Job", default: [] },
-
 	cgpa: { type: Number, min: 0, max: 10 },
 	skills: { type: [String], default: [] },
 	experience: { type: String },
@@ -33,6 +44,35 @@ const userSchema = new Schema({
 		default: "",
 	},
 	createdAt: { type: Date, default: Date.now },
+});
+
+userSchema.index(
+	{ enrollmentNumber: 1 },
+	{
+		unique: true,
+		partialFilterExpression: { userType: "student" },
+	}
+);
+
+userSchema.pre("save", function (next) {
+	if (this.userType !== "student") {
+		this.enrollmentNumber = undefined;
+		this.branch = undefined;
+		this.passingYear = undefined;
+		this.resumeLink = undefined;
+		this.appliedJobs = undefined;
+		this.cgpa = undefined;
+		this.skills = undefined;
+		this.experience = undefined;
+		this.certifications = undefined;
+		this.projects = undefined;
+		this.contactNumber = undefined;
+		this.linkedinProfile = undefined;
+		this.portfolioLink = undefined;
+		this.placementStatus = undefined;
+		this.company = undefined;
+	}
+	next();
 });
 
 module.exports = mongoose.model("User", userSchema);
